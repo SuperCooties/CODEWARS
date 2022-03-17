28def2aba84bec9447d5f4084983076a1d74ce44
Cooties <aaronmrc@gmail.com>
2022-03-16 23:00:40 -0700

    8kyu sol 59

---
+++ 8kyu/solution0059/count-the-monkeys.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+function monkeyCount(n) {
+    let arr = [];
+    
+    for (i = 1; i <= n; i++){
+      arr.push(i);
+    }
+    
+    return arr;
+  }
