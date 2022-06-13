551cf85eb2f8de90b66c8a9aef119e295f11762c
Cooties <aaronmrc@gmail.com>
2022-06-12 22:45:20 -0700

    8kyu sol 128

---
+++ 8kyu/solution0128/multiplication-table-for-number.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
+
+function multiTable(number) {
+    return `1 * ${number} = ${1 * number}\n` + 
+           `2 * ${number} = ${2 * number}\n` +
+           `3 * ${number} = ${3 * number}\n` +
+           `4 * ${number} = ${4 * number}\n` +
+           `5 * ${number} = ${5 * number}\n` +
+           `6 * ${number} = ${6 * number}\n` +
+           `7 * ${number} = ${7 * number}\n` +
+           `8 * ${number} = ${8 * number}\n` +
+           `9 * ${number} = ${9 * number}\n` +
+           `10 * ${number} = ${10 * number}`
+  }
