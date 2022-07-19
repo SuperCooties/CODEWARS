9e9d3171042aeef240dd8d16f51398e7ba8ac7c9
Cooties <aaronmrc@gmail.com>
2022-07-18 22:53:32 -0700

    8kyu sol 141

---
+++ 8kyu/solution0141/generate-range-of-integers.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/55eca815d0d20962e1000106
+
+function generateRange(min, max, step){
+    let result = [];
+    
+    for (let i = min; i <= max; i += step) {
+      result.push(i);
+    }
+    
+    return result;
+  }
