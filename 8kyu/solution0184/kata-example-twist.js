d1be8f608b8e63ccd56cf1cfcf19f2269327ba4e
Cooties <aaronmrc@gmail.com>
2022-08-29 20:32:58 -0700

    8kyu sol 184

---
+++ 8kyu/solution0184/kata-example-twist.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/525c1a07bb6dda6944000031
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+const loops = 1000;
+const name = "codewars"
+
+for (let i = 0; i < loops; i++) {
+  websites.push(name);
+}
+
