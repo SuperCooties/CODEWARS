3ebf38011985fa8420f96394a2d69a021dfd7abf
Cooties <aaronmrc@gmail.com>
2022-09-17 22:51:44 -0700

    8kyu sol 204

---
+++ 8kyu/solution0204/multiple-of-index.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/5a34b80155519e1a00000009
+
+function multipleOfIndex(array) {
+    let newArr = [];
+    
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] % i === 0) {
+        newArr.push(array[i]);
+      }
+    }
+    return newArr;
+  }
