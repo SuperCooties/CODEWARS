d03f9c1d2b8cba0d5b554a93128792fc7ebe3ef0
Cooties <aaronmrc@gmail.com>
2022-10-28 00:09:46 -0700

    8kyu sol 257

---
+++ 8kyu/solution0257/bug-fixing.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/55c933c115a8c426ac000082
+
+function evalObject(value){
+    let result = 0;
+    switch(value.operation){
+      case '+': result = value.a + value.b; break;
+      case '-': result = value.a - value.b; break;
+      case '/': result = value.a / value.b; break;
+      case '*': result = value.a * value.b; break;
+      case '%': result = value.a % value.b; break;
+      case '^': result = Math.pow(value.a, value.b); break;
+    }
+    return result;
+  }
